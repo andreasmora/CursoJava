52f9de7acaa1f5d728c7ce981e3888dfd07a534a
andreasmora <andrealvesmoraes75@gmail.com>
2025-05-28 11:43:39 -0300

    Aulas em Java

--- JavaAulas/src/com/java/HelloWord.java
+++ JavaAulas/src/com/java/HelloWord.java
@@ -2,6 +2,6 @@ package com.java;
 
 public class HelloWord {
     public static void main(String[] args) {
-        System.out.println("Hello Word!");
+        System.out.println("Hello Word!!!");
     }
 }
