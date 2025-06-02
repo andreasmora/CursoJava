1fca4b6cb94825811d24618356cd3fcd072f991c
andreasmora <andrealvesmoraes75@gmail.com>
2025-06-02 16:20:24 -0300

    Curso em Java

---
+++ JavaAulas/src/Maratona_Java/TiposPrimitivos.java
@@ -0,0 +1,17 @@
+package Maratona_Java;
+
+// int, char, double, float, boolean, short, long, byte
+public class TiposPrimitivos {
+    public static void main(String[] args) {
+
+        int idade = 20;
+        String nome = "Andre";
+        String nomeEsposa = "Aline";
+        String nomeCachorro = "Jan Jan";
+
+        System.out.println(nome);
+        System.out.println(nomeEsposa);
+        System.out.println(nomeCachorro);
+
+    }
+}
