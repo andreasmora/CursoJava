e879a5f83b38d576608ca591f0767135ccbfeb72
andreasmora <andrealvesmoraes75@gmail.com>
2025-06-05 15:32:52 -0300

    Curso em Java

--- JavaAulas/src/Maratona_Java/LeituraTeclado.java
+++ JavaAulas/src/Maratona_Java/LeituraTeclado.java
@@ -6,10 +6,22 @@ public class LeituraTeclado {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        System.out.println("Digite seu nome: ");
-        String nome = scan.next();
-        System.out.println("Bem vindo!!! " + nome);
+        System.out.println("Digite seu primeiro nome: ");
+        String primeiroNome = scan.nextLine();
+        System.out.println("Seu nome é: " + primeiroNome);
 
+        System.out.println("Digite seu sobrenome: ");
+        String sobreNome = scan.next();
+        System.out.println("Seu sobrenme é: " +sobreNome);
+        System.out.println(primeiroNome + sobreNome);
+
+        System.out.println("Digite sua idade: ");
+        int idade = scan.nextInt();
+        System.out.println("Sua idade é: " + idade);
+
+        System.out.println("Digite sua altura: ");
+        double altura = scan.nextDouble();
+        System.out.println("Sua altura é: " + altura);
 
     }
 }
