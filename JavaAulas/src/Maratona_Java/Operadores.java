e0cb59ba3c7572e759fcaeebeddd88e9d324e28b
andreasmora <andrealvesmoraes75@gmail.com>
2025-06-05 16:47:01 -0300

    Curso em Java

---
+++ JavaAulas/src/Maratona_Java/Operadores.java
@@ -0,0 +1,27 @@
+package Maratona_Java;
+
+import java.util.Scanner;
+
+public class Operadores {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int resultado = 1 + 2;
+        System.out.println("O resultado é: " + resultado);
+
+        resultado = resultado + 1;
+        System.out.println(resultado);
+
+        resultado = resultado - 1;
+        System.out.println(resultado);
+
+        resultado = resultado * 2;
+        System.out.println(resultado);
+
+        resultado = resultado / 2;
+        System.out.println(resultado);
+
+
+
+    }
+}
