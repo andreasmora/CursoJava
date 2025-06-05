a8121d48f859871458ec7db571f0ccdfbcb9583f
andreasmora <andrealvesmoraes75@gmail.com>
2025-06-05 16:07:44 -0300

    Curso em Java

--- JavaAulas/src/Maratona_Java/LeituraTeclado.java
+++ JavaAulas/src/Maratona_Java/LeituraTeclado.java
@@ -6,7 +6,7 @@ public class LeituraTeclado {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        System.out.println("Digite seu primeiro nome: ");
+        /*System.out.println("Digite seu primeiro nome: ");
         String primeiroNome = scan.nextLine();
         System.out.println("Seu nome é: " + primeiroNome);
 
@@ -21,7 +21,20 @@ public class LeituraTeclado {
 
         System.out.println("Digite sua altura: ");
         double altura = scan.nextDouble();
-        System.out.println("Sua altura é: " + altura);
+        System.out.println("Sua altura é: " + altura); */
 
+        System.out.println("Entre com seu nome, sua idade, sua altura, seu peso e se tem filhos: ");
+        String nome = scan.next();
+        int idade = scan.nextInt();
+        double altura = scan.nextDouble();
+        float peso = scan.nextFloat();
+        boolean filhos = scan.nextBoolean();
+
+
+        System.out.println("Seu nome é: " + nome);
+        System.out.println("Sua idade é: " + idade);
+        System.out.println("Sua altura é: " + altura);
+        System.out.println("Seu peso é: " + peso);
+        System.out.println("Tem filhos? " + filhos);
     }
 }
